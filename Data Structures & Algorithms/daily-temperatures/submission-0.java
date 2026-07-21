5c4fb3d1ad681997cff2fbe4b63479ea1ba4906b
Sebastian Ezpeleta-Stewart <49777061+seabass42@users.noreply.github.com>
2026-07-20 17:35:47 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++){
+            result[i] = 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++){
+            while (!stack.isEmpty() && stack.peek() < temperatures[i]){
+                int element = stack.pop();
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+            stack.push(temperatures[i]);
+        }
+        return result;
+    }
+}
