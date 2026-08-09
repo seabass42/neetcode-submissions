422bfd587c15baaac793d99bd03a82d0dfbe2345
Sebastian Ezpeleta-Stewart <49777061+seabass42@users.noreply.github.com>
2026-08-08 17:14:19 -0700

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = 0;
+        while (left <= right){
+            middle = (left + right + 1) / 2;
+            if (nums[middle] == target){
+                return middle;
+            } else if (nums[middle] < target){
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
